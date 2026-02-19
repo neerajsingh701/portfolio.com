498a85a358cac9fbf95bc8269519fcffb1c67a21
Neeraj singh <royneeraj521@gmail.com>
2026-02-19 09:55:41 +0530

    Add all files

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://neerajportfolio-com.onrender.com/api"
 })
 
 export default instance
